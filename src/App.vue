e70b077902a99b34cd2228230c4e6052363cb49b
Alessio1921 <caruso.alessio1999@gmail.com>
2022-03-25 16:32:36 +0100

    create componet title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <titleHeader msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import titleHeader from "./components/titleHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    titleHeader,
   },
 };
 </script>
